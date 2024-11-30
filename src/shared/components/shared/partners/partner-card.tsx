b73979c05900da0416d9fa3028e0037066e5d200
sydykovkanat <153425875+sydykovkanat@users.noreply.github.com>
2024-11-30 16:02:22 +0600

    Use Image from next/image (#8)
    * realized partners
    
    * fix partners container
    
    * use Image components
    
    ---------
    
    Co-authored-by: Kanat Sydykov <kanatsydykov@MacBook-Air-Kanat.local>

--- src/shared/components/shared/partners/partner-card.tsx
+++ src/shared/components/shared/partners/partner-card.tsx
@@ -1,6 +1,7 @@
 import { API_URL } from '@/shared/constants';
 import { cn } from '@/shared/lib/utils';
 import { Partner } from '@/shared/types/partner.types';
+import Image from 'next/image';
 
 import React from 'react';
 
@@ -14,7 +15,7 @@ interface Props {
 export const PartnerCard: React.FC<Props> = ({ className, partner }) => {
   return (
     <a href={partner.url} className={cn(styles.card, className)}>
-      <img src={`${API_URL}/${partner.image}`} alt={partner.name} />
+      <Image width={120} height={100} src={`${API_URL}/${partner.image}`} alt={partner.name} />
       <span className={'sr-only'}>{partner.name} partner logo</span>
     </a>
   );
