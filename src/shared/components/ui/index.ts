b37e4489630a0e986b5c03ee034b60a787635c7c
sydykovkanat <153425875+sydykovkanat@users.noreply.github.com>
2024-12-02 21:01:22 +0600

    implement login (#18)
    Co-authored-by: Kanat Sydykov <kanatsydykov@MacBook-Air-Kanat.local>

--- src/shared/components/ui/index.ts
+++ src/shared/components/ui/index.ts
@@ -1,2 +1,5 @@
 export { Button } from './button';
 export { Card } from './card';
+export { Input } from './input';
+export { Label } from './label';
+export { Toaster } from './sonner';
---
+++ src/shared/hooks/hooks.ts
@@ -0,0 +1,7 @@
+import { AppDispatch, AppStore, RootState } from '@/shared/lib/store';
+
+import { useDispatch, useSelector, useStore } from 'react-redux';
+
+export const useAppDispatch = useDispatch.withTypes<AppDispatch>();
+export const useAppSelector = useSelector.withTypes<RootState>();
+export const useAppStore = useStore.withTypes<AppStore>();
