c9b9181bb0adb88a11642ccbc8e1525bf6c891d7
Muhtarov Nurdin <nurdinmuhtarovv@gmail.com>
2024-12-04 21:36:43 +0600

    TC#69 I started dividing the admin carousel component into smaller components

---
+++ src/shared/components/shared/carousel/admin/form-carousel-for-admin/form-carousel-for-admin.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const FormCarouselForAdmin = () => {
+  return <div></div>;
+};
---
+++ src/shared/components/shared/carousel/admin/header-carousel-for-admin/header-carousel-for-admin.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const HeaderCarouselForAdmin = () => {
+  return <div></div>;
+};
