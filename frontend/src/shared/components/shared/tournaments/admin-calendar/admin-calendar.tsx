3d0de7b1466b86b0b3d3e0d7cb0eb1dedcb3f11f
Alikhan Kubatbekov <alixan.kubatbekov@gmail.com>
2024-12-12 13:09:22 +0600

    TC#79: Some little changes

--- frontend/src/shared/components/shared/tournaments/admin-calendar/admin-calendar.tsx
+++ frontend/src/shared/components/shared/tournaments/admin-calendar/admin-calendar.tsx
@@ -2,7 +2,7 @@
 
 import { AdminPageHeader, TournamentCalendar } from '@/shared/components/shared';
 import { TournamentForm } from '@/shared/components/shared/tournaments';
-import { useTournaments } from '@/shared/components/shared/tournaments/hooks';
+import {useTournamentForm, useTournaments} from '@/shared/components/shared/tournaments/hooks';
 import { Button } from '@/shared/components/ui';
 import { Tournaments } from '@/shared/types/tournament.types';
 import { SquaresPlusIcon } from '@heroicons/react/24/outline';
@@ -17,15 +17,15 @@ interface Props {
 
 export const AdminCalendar: React.FC<Props> = ({ tournaments }) => {
   const { tournamentsFetching, tournamentsLastYearExist, tournamentsNextYearExist } = useTournaments();
+  const { open, setOpen } = useTournamentForm();
 
   return (
     <>
       <AdminPageHeader title='Календарь' description='Управление турнирами'>
-        <TournamentForm tournamentsLastYearExist={tournamentsLastYearExist}>
-          <Button className={styles.addButton} icon={SquaresPlusIcon}>
-            Создать турнир
+          {open && <TournamentForm tournamentsLastYearExist={tournamentsLastYearExist} open={open} setOpen={setOpen}/>}
+          <Button className={styles.addButton} icon={SquaresPlusIcon} onClick={() => setOpen(true)}>
+              Создать турнир
           </Button>
-        </TournamentForm>
       </AdminPageHeader>
       <TournamentCalendar
         tournaments={tournaments}
