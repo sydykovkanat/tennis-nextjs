433e9b234a8541560b2da0d9259b4d260a490573
Gordus Alexander <aldrmiller@gmail.com>
2024-12-27 23:07:12 +0600

    some refac

--- frontend/src/app/(root)/admin/page.tsx
+++ frontend/src/app/(root)/admin/page.tsx
@@ -19,14 +19,9 @@ const ProtectedRoute = dynamic(() => import('@/shared/components/shared/protecte
 
 export default function Page({ searchParams: params }: { searchParams: { [key: string]: string } }) {
   const userPermission = useAppSelector(selectUserPermission);
-  const [isClient, setIsClient] = useState(false);
 
   const searchParams = new URLSearchParams(params);
   const router = useRouter();
-
-  useEffect(() => {
-    setIsClient(true);
-  }, []);
 
   const defaultTab = 'partners';
   const [currentTab, setCurrentTab] = useState(searchParams.get('tab') || defaultTab);
@@ -44,62 +39,51 @@ export default function Page({ searchParams: params }: { searchParams: { [key: s
   };
 
   return (
-    <>
-      {isClient && (
-        <>
-          <Suspense>
-            <ProtectedRoute isAllowed={userPermission >= 2}>
-              <Container>
-                <h1 className={cn(styles.title)}>Панель Администратора</h1>
-                <Tabs
-                  value={currentTab}
-                  orientation={'vertical'}
-                  defaultValue={defaultTab}
-                  onValueChange={handleTabChange}
-                >
-                  <ScrollArea className={cn(styles.scroll)}>
-                    <TabsList className={cn(styles.tabsList, 'dark:bg-[#1F2937]')}>
-                      {ADMIN_PAGES.map((page, i) => (
-                        <TabsTrigger key={i} value={page.value}>
-                          {page.name}
-                        </TabsTrigger>
-                      ))}
-                    </TabsList>
-                    <ScrollBar orientation={'horizontal'} />
-                  </ScrollArea>
-                  <TabsContent value={'calendar'}>
-                    <Calendar />
-                  </TabsContent>
-                  <TabsContent value={'carousel'}>
-                    <Carousel />
-                  </TabsContent>
-                  <TabsContent value={'rating'}>
-                    <Rating />
-                  </TabsContent>
-                  <TabsContent value={'news'}>
-                    <News />
-                  </TabsContent>
-                  <TabsContent value={'footer'}>
-                    <Footer />
-                  </TabsContent>
-                  <TabsContent value={'partners'}>
-                    <Partners />
-                  </TabsContent>
-                  <TabsContent value={'top'}>
-                    <Top />
-                  </TabsContent>
-                  <TabsContent value={'category'}>
-                    <Category />
-                  </TabsContent>
-                  <TabsContent value={'users'}>
-                    <Users />
-                  </TabsContent>
-                </Tabs>
-              </Container>
-            </ProtectedRoute>
-          </Suspense>
-        </>
-      )}
-    </>
+    <ProtectedRoute isAllowed={userPermission >= 2}>
+      <Suspense>
+        <Container>
+          <h1 className={cn(styles.title)}>Панель Администратора</h1>
+          <Tabs value={currentTab} orientation={'vertical'} defaultValue={defaultTab} onValueChange={handleTabChange}>
+            <ScrollArea className={cn(styles.scroll)}>
+              <TabsList className={cn(styles.tabsList, 'dark:bg-[#1F2937]')}>
+                {ADMIN_PAGES.map((page, i) => (
+                  <TabsTrigger key={i} value={page.value}>
+                    {page.name}
+                  </TabsTrigger>
+                ))}
+              </TabsList>
+              <ScrollBar orientation={'horizontal'} />
+            </ScrollArea>
+            <TabsContent value={'calendar'}>
+              <Calendar />
+            </TabsContent>
+            <TabsContent value={'carousel'}>
+              <Carousel />
+            </TabsContent>
+            <TabsContent value={'rating'}>
+              <Rating />
+            </TabsContent>
+            <TabsContent value={'news'}>
+              <News />
+            </TabsContent>
+            <TabsContent value={'footer'}>
+              <Footer />
+            </TabsContent>
+            <TabsContent value={'partners'}>
+              <Partners />
+            </TabsContent>
+            <TabsContent value={'top'}>
+              <Top />
+            </TabsContent>
+            <TabsContent value={'category'}>
+              <Category />
+            </TabsContent>
+            <TabsContent value={'users'}>
+              <Users />
+            </TabsContent>
+          </Tabs>
+        </Container>
+      </Suspense>
+    </ProtectedRoute>
   );
 }
