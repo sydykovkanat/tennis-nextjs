cb418e8cbc96cc0e27ad66433ce4b3386fcb4a03
Muhtarov Nurdin <nurdinmuhtarovv@gmail.com>
2025-01-10 03:08:13 +0600

    Tc#83 added text to the admin panel, no logos yet

--- frontend/src/shared/components/shared/footer/admin/components/main-logo/main-logo-cards/main-logo-cards.tsx
+++ frontend/src/shared/components/shared/footer/admin/components/main-logo/main-logo-cards/main-logo-cards.tsx
@@ -15,34 +15,46 @@ export const MainLogoCards = () => {
     const { logos, itemsLoading, handleLogoClick, handleDeleteLogo } = useMainLogoCards();
 
     return (
-        <div className={cn(styles.containerMainLogoItem)}>
+        <div className={cn(logos.length === 0 ? styles.containerMainLogoItemNoText : styles.containerMainLogoItem)}>
             {itemsLoading ? (
-                <Loader/>
-            ) :  logos?.map((logo) => (
-                <div key={logo._id}
-                     className={cn(styles.cardMainLogo, 'dark:bg-[#1F2937]',)}
-                     onClick={() => handleLogoClick(logo._id)}
-                     data-test-id='update-logo-header'
-                >
-                    <div>
-                        <img className={cn(styles.image)}
-                             src={API_URL + '/' + logo.logo}
-                             alt={logo._id}
-                        />
-                    </div>
-
-                    <Confirm onOk={() => handleDeleteLogo(logo._id)}>
-                        <div className={cn(styles.btnDelete)} onClick={(e) => {
-                            e.stopPropagation();
-                        }}>
-                            <Button  data-test-id='delete-logo-header'> <Trash/> </Button>
-                        </div>
-                    </Confirm>
-
+                <div>
+                    <Loader/>
                 </div>
+            ) : logos.length === 0 ? (
+                <small className={styles.noLogoText}> Логотипы не были найдены </small>
+            ) : (
+                logos.map((logo) => (
+                    <div
+                        key={logo._id}
+                        className={cn(styles.cardMainLogo, 'dark:bg-[#1F2937]')}
+                        onClick={() => handleLogoClick(logo._id)}
+                        data-test-id='update-logo-header'
+                    >
+                        <div>
+                            <img
+                                className={cn(styles.image)}
+                                src={API_URL + '/' + logo.logo}
+                                alt={logo._id}
+                            />
+                        </div>
 
-            ))}
+                        <Confirm onOk={() => handleDeleteLogo(logo._id)}>
+                            <div
+                                className={cn(styles.btnDelete)}
+                                onClick={(e) => {
+                                    e.stopPropagation();
+                                }}
+                            >
+                                <Button data-test-id='delete-logo-header'>
+                                    <Trash/>
+                                </Button>
+                            </div>
+                        </Confirm>
+                    </div>
+                ))
+            )}
         </div>
+
     );
 };
 
